091813bb0bac8975dbf36bf48d36ff13de08c3f9
h2cone <tw8ape@gmail.com>
2025-05-25 23:39:25 +0800

    Enhance player functionality by adding animation handling and movement logic in player.rs, and update player.tscn with new animations and properties for improved gameplay experience.

--- rust/src/player/player.rs
+++ rust/src/player/player.rs
@@ -1,19 +1,37 @@
 use godot::{
-    classes::{CharacterBody2D, ICharacterBody2D, ProjectSettings},
+    classes::{
+        AnimationPlayer, CharacterBody2D, ICharacterBody2D, Input, ProjectSettings, Sprite2D,
+    },
     prelude::*,
 };
+
+const WALK_SPEED: f32 = 300.0;
+
+enum State {
+    Air,
+    Floor,
+}
 
 #[derive(GodotClass)]
 #[class(base=CharacterBody2D)]
 pub struct Player {
     base: Base<CharacterBody2D>,
     gravity: f64,
+    state: State,
+    sprite: OnReady<Gd<Sprite2D>>,
+    animation_player: OnReady<Gd<AnimationPlayer>>,
 }
 
 #[godot_api]
 impl ICharacterBody2D for Player {
     fn init(base: Base<CharacterBody2D>) -> Self {
-        Self { base, gravity: 0.0 }
+        Self {
+            base,
+            gravity: 0.0,
+            state: State::Air,
+            sprite: OnReady::from_node("Sprite2D"),
+            animation_player: OnReady::from_node("AnimationPlayer"),
+        }
     }
 
     fn ready(&mut self) {
@@ -24,7 +42,64 @@ impl ICharacterBody2D for Player {
     fn physics_process(&mut self, delta: f64) {
         let mut velocity = self.base().get_velocity();
         velocity.y += self.gravity as f32 * delta as f32;
+
+        match self.state {
+            State::Air => {
+                self.movement(&mut velocity);
+                if self.base().is_on_floor() {
+                    self.state = State::Floor;
+                }
+            }
+            State::Floor => {
+                self.movement(&mut velocity);
+            }
+        }
+
         self.base_mut().set_velocity(velocity);
         self.base_mut().move_and_slide();
+
+        if !self.base().get_velocity().x.is_zero_approx() {
+            let mut scale = self.sprite.get_scale();
+            if self.base().get_velocity().x > 0. {
+                scale.x = 1.;
+            } else {
+                scale.x = -1.;
+            }
+            self.sprite.set_scale(scale);
+        }
+        let animation = self.get_new_animation();
+        if animation != self.animation_player.get_current_animation() {
+            self.animation_player.set_current_animation(&animation);
+            self.animation_player.play();
+        }
+    }
+}
+
+impl Player {
+    fn movement(&mut self, velocity: &mut Vector2) {
+        let input = Input::singleton();
+        if input.is_action_pressed("ui_right") {
+            velocity.x = WALK_SPEED;
+        } else if input.is_action_pressed("ui_left") {
+            velocity.x = -WALK_SPEED;
+        } else {
+            velocity.x = 0.;
+        }
+    }
+
+    fn get_new_animation(&self) -> GString {
+        if self.base().is_on_floor() {
+            return if self.base().get_velocity().abs().x > 0.1 {
+                GString::from("run")
+            } else {
+                GString::from("idle")
+            };
+        } else {
+            return if self.base().get_velocity().y > 0. {
+                GString::from("falling")
+            } else {
+                GString::from("jumping")
+            };
+        }
     }
 }
