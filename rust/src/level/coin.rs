c75d3193558f08545ac6272e4caddbf508f6c16d
h2cone <tw8ape@gmail.com>
2025-08-10 16:38:41 +0800

    Add coin functionality to the level by introducing a new Coin node with animation support. Update level.tscn to include coin instances and adjust the scene structure. Modify Rust coin implementation to handle body interactions and trigger animations upon collection.

--- rust/src/level/coin.rs
+++ rust/src/level/coin.rs
@@ -1,5 +1,5 @@
 use godot::{
-    classes::{Area2D, IArea2D},
+    classes::{AnimationPlayer, Area2D, IArea2D},
     prelude::*,
 };
 
@@ -7,11 +7,28 @@ use godot::{
 #[class(base=Area2D)]
 pub struct Coin {
     base: Base<Area2D>,
+    animation_player: OnReady<Gd<AnimationPlayer>>,
 }
 
 #[godot_api]
 impl IArea2D for Coin {
     fn init(base: Base<Area2D>) -> Self {
-        Self { base }
+        Self {
+            base,
+            animation_player: OnReady::from_node("AnimationPlayer"),
+        }
+    }
+
+    fn ready(&mut self) {
+        self.base()
+            .signals()
+            .body_entered()
+            .connect_other(self, Self::on_body_entered);
+    }
+}
+
+impl Coin {
+    fn on_body_entered(&mut self, _body: Gd<Node2D>) {
+        self.animation_player.set_current_animation("picked");
     }
 }
